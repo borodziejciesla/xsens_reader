d041b26e028be1c212f4f784b1849bd20deda54a
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2021-06-06 21:23:51 +0200

    Added cmake

---
+++ include/xsens_reader.h
@@ -0,0 +1,15 @@
+#ifndef XSENS_READER_INCLUDE_XSENS_READER_H_
+#define XSENS_READER_INCLUDE_XSENS_READER_H_
+
+namespace xsens_reader
+{
+    class XsensReader
+    {
+        public:
+            XsensReader(void) = default;
+            ~XsensReader(void) = default;
+        private:
+    };
+}
+
+#endif //XSENS_READER_INCLUDE_XSENS_READER_H_
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+
+#include "xsens_reader.h"
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
